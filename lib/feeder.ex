f251d7aa26633934cc232fd6e502cb55791a4781
oskimura <oskimura@gmail.com>
2017-11-20 22:33:59 +0900

    add http get

--- lib/feeder.ex
+++ lib/feeder.ex
@@ -1,2 +1,13 @@
 defmodule Feeder do
+
+  # urlに指定されたサイトを読み込む
+  def getRssFeedXml(url) do
+    url = "http://tech.uzabase.com/rss"
+    HTTPoison.get!(url)
+  end
+
+  # httpレスポンスがbodyを取得する
+  def getBody(response) do
+    response.body
+  end
 end
